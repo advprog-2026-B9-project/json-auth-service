cd33c0a1162aa003fc801bed8b75323434c9f6d4
farishuda <farishudaku@gmail.com>
2026-04-17 20:07:09 +0700

    test: integration testing for repository, service, and controller

--- src/test/java/com/b9/json/jsonplatform/auth/domain/UserTest.java
+++ src/test/java/com/b9/json/jsonplatform/auth/domain/UserTest.java
@@ -1,6 +1,9 @@
 package com.b9.json.jsonplatform.auth.domain;
 
 import org.junit.jupiter.api.Test;
+
+import java.util.UUID;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class UserTest {
@@ -8,12 +11,13 @@ class UserTest {
     @Test
     void testUserGettersAndSetters() {
         User user = new User();
-        user.setId(1L);
+        UUID id = UUID.randomUUID();
+        user.setId(id);
         user.setEmail("mahasiswa@ui.ac.id");
         user.setPassword("rahasia123");
         user.setUsername("mahasiswa_ui");
 
-        assertEquals(1L, user.getId());
+        assertEquals(id, user.getId());
         assertEquals("mahasiswa@ui.ac.id", user.getEmail());
         assertEquals("rahasia123", user.getPassword());
         assertEquals("mahasiswa_ui", user.getUsername());
