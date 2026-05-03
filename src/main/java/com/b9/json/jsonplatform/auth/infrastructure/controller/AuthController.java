fcdab82d50fa7d781482af4596ec126f79e06918
RafasyaSubhan <rafasya.subhan@gmail.com>
2026-05-03 14:12:14 +0700

    refactor: change endpoint prefix from /auth to /api/auth

--- src/main/java/com/b9/json/jsonplatform/auth/infrastructure/controller/AuthController.java
+++ src/main/java/com/b9/json/jsonplatform/auth/infrastructure/controller/AuthController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/auth")
+@RequestMapping("/api/auth")
 public class AuthController {
 
     @Autowired
