b0e1608f6322a5cedc6defca4f52b488d6ef6f25
RafasyaSubhan <rafasya.subhan@gmail.com>
2026-05-22 01:02:13 +0700

    refactor: remove CorsConfig and disable local CORS to delegate to API Gateway

--- src/main/java/com/b9/json/jsonplatform/global/config/SecurityConfig.java
+++ src/main/java/com/b9/json/jsonplatform/global/config/SecurityConfig.java
@@ -17,7 +17,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .cors(Customizer.withDefaults())
+                .cors(AbstractHttpConfigurer::disable)
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/v1/auth/**").permitAll()
